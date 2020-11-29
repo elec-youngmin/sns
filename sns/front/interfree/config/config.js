3816fca1b2499b0509db1eb0e377027bec84c362
mintZeroCode <mintzerocode@gmail.com>
2020-11-30 01:41:29 +0900

    'config.js'

--- sns/front/interfree/config/config.js
+++ sns/front/interfree/config/config.js
@@ -1,2 +1,5 @@
-exports.frontUrl = "http://3.34.241.174";
+exports.frontUrl = "http://interfree.co.kr/";
 exports.backUrl = "http://3.35.142.52";
+
+// exports.backUrl = "http://localhost:80";
+// exports.frontUrl = "http://3.34.241.174";
